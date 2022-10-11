7e38f4907347aef1a055fbedcbf7dea9903cc18a
abdul-abstractit <ars@abstractit.in>
2022-10-11 18:12:03 +0530

    Refactored googleAuth.

--- controller/googleAuth.js
+++ controller/googleAuth.js
@@ -1,40 +1,39 @@
 const axios = require('axios')
 const { google } = require('googleapis')
 
-const googleAuth = () => {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.OAUTH_RETURN_URL
-  );
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.OAUTH_RETURN_URL
+);
 
-  return {
-    getGoogleAuthURL: () => {
-      const scopes = [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email',
-      ];
+const googleAuth = () => ({
+  getGoogleAuthURL: () => {
+    const scopes = [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
+    ];
 
-      return oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        prompt: 'consent',
-        scope: scopes,
-      });
-    },
+    return oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      prompt: 'consent',
+      scope: scopes,
+    });
+  },
 
-    getGoogleUser: async ({ code }) => {
-      const { tokens } = await oauth2Client.getToken(code);
-      const res = await axios(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`,
-        {
-          headers: {
-            Authorization: `Bearer ${tokens.id_token}`,
-          },
+  getGoogleUser: async ({ code }) => {
+    const { tokens } = await oauth2Client.getToken(code);
+    const authUser = await axios(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${tokens.id_token}`,
         },
-      )
+      },
+    )
 
-      return res.data;
-    }
+    return authUser.data;
   }
-}
+})
+
 
 module.exports = googleAuth
